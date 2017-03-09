408fa33900f66280812c9d367cc4788fed2d1717
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-03-09 16:33:05 +0800

    测试类

---
+++ yc-translator-core/src/test/java/com/ai/yc/user/test/TestDubboService.java
@@ -0,0 +1,14 @@
+package com.ai.yc.user.test;
+
+import org.junit.Test;
+
+import com.ai.opt.sdk.appserver.DubboServiceStart;
+
+public class TestDubboService {
+
+	@Test
+	public void testDubboStart(){
+        DubboServiceStart.main(null);
+    }
+
+}

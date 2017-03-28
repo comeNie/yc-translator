a733acb6bc50f386f91fa1f7916e6a131751e409
hexf3 <hexf3@10.1.54.37>
2017-03-28 10:28:10 +0800

    新增接口

--- yc-translator-api/src/main/java/com/ai/yc/translator/api/translatorservice/interfaces/ITranslatorInfoServiceSV.java
+++ yc-translator-api/src/main/java/com/ai/yc/translator/api/translatorservice/interfaces/ITranslatorInfoServiceSV.java
@@ -1,13 +1,16 @@
 package com.ai.yc.translator.api.translatorservice.interfaces;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
+import com.ai.opt.base.vo.PageInfo;
 import com.ai.yc.translator.api.translatorservice.param.TranslatorInfoQueryRequest;
+import com.ai.yc.translator.api.translatorservice.param.TranslatorInfoQueryResponse;
 import com.ai.yc.translator.api.translatorservice.param.TranslatorInfoResponse;
 
 @Path("/translatorInfoService")
@@ -18,5 +21,7 @@ public interface ITranslatorInfoServiceSV {
    * 查询译员列表信息
    * 
    */
-	TranslatorInfoResponse  queryTranslatorInfo(TranslatorInfoQueryRequest param) throws BusinessException,SystemException;
+	@POST
+	@Path("/queryTranslatorInfo")
+	TranslatorInfoQueryResponse queryTranslatorInfo(TranslatorInfoQueryRequest param) throws BusinessException,SystemException;
 }

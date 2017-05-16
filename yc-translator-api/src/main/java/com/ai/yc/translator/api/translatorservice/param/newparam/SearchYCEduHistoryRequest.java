ccc0ce193320e8db9776964493c24c0b15e1acf0
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-05-16 16:59:15 +0800

    修改对象

--- yc-translator-api/src/main/java/com/ai/yc/translator/api/translatorservice/param/newparam/SearchYCEduHistoryRequest.java
+++ yc-translator-api/src/main/java/com/ai/yc/translator/api/translatorservice/param/newparam/SearchYCEduHistoryRequest.java
@@ -4,13 +4,15 @@ import java.io.Serializable;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+import com.ai.opt.base.vo.BaseResponse;
+
 /** 
  * @author  作者 “WTF” E-mail: 1031248990@qq.com
  * @date 创建时间：2016年12月2日 下午3:55:28 
  * @version 
  * @since  
  */
-public class SearchYCEduHistoryRequest implements Serializable {
+public class SearchYCEduHistoryRequest extends BaseResponse {
 
 	/**
 	 * 

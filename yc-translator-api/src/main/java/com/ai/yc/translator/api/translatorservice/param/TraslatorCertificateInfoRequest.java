e036451dbf2b9e8e3d85d0472308d111d99ba2fe
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-05-18 23:02:34 +0800

    资质入参

---
+++ yc-translator-api/src/main/java/com/ai/yc/translator/api/translatorservice/param/TraslatorCertificateInfoRequest.java
@@ -0,0 +1,32 @@
+package com.ai.yc.translator.api.translatorservice.param;
+
+import java.util.List;
+
+import com.ai.yc.translator.api.translatorservice.param.newparam.InsertYCWorkExprienceRequest;
+import com.ai.yc.translator.api.translatorservice.param.newparam.UsrCertificateMessage;
+import com.ai.yc.translator.api.translatorservice.param.newparam.UsrEducationMessage;
+
+public class TraslatorCertificateInfoRequest {
+	List<UsrEducationMessage> eduResultList;
+	List<InsertYCWorkExprienceRequest> workResultList;
+	List<UsrCertificateMessage> cardResultList;
+	public List<UsrEducationMessage> getEduResultList() {
+		return eduResultList;
+	}
+	public void setEduResultList(List<UsrEducationMessage> eduResultList) {
+		this.eduResultList = eduResultList;
+	}
+	public List<InsertYCWorkExprienceRequest> getWorkResultList() {
+		return workResultList;
+	}
+	public void setWorkResultList(List<InsertYCWorkExprienceRequest> workResultList) {
+		this.workResultList = workResultList;
+	}
+	public List<UsrCertificateMessage> getCardResultList() {
+		return cardResultList;
+	}
+	public void setCardResultList(List<UsrCertificateMessage> cardResultList) {
+		this.cardResultList = cardResultList;
+	}
+	
+}

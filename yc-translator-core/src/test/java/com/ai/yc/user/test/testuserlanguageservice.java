8014da5f6cb16924f05ef942e552dbff8df0cc40
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-05-21 23:16:44 +0800

    语言对测试接口

---
+++ yc-translator-core/src/test/java/com/ai/yc/user/test/testuserlanguageservice.java
@@ -0,0 +1,29 @@
+package com.ai.yc.user.test;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ai.yc.translator.api.userlanguage.interfaces.IYCUserLanguageSV;
+import com.ai.yc.translator.api.userlanguage.param.UsrLanguageRequest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/context/core-context.xml" })
+public class testuserlanguageservice {
+	@Autowired
+	private IYCUserLanguageSV userLanguageSV;
+	@Test
+	public void testLanguageInsert(){
+		UsrLanguageRequest languageRequest = new UsrLanguageRequest();
+		languageRequest.setDuadId("1");
+		languageRequest.setReferencePrice(new BigDecimal(200));
+		languageRequest.setTranslationType("0");
+		languageRequest.setUserId("525335");
+		languageRequest.setState("0");
+		userLanguageSV.insertLanguageInfo(languageRequest);
+	}
+}

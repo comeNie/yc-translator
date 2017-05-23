f42d5a4ba1e4f076a02b6637646cd62b58549cd7
zhanglh <zhanglh@asiainfo.com>
2017-05-23 15:04:50 +0800

    修改引用包

--- yc-translator-core/src/main/java/com/ai/yc/translator/api/allointerper/impl/AlloTranslatorServiceSVImpl.java
+++ yc-translator-core/src/main/java/com/ai/yc/translator/api/allointerper/impl/AlloTranslatorServiceSVImpl.java
@@ -3,8 +3,8 @@ package com.ai.yc.translator.api.allointerper.impl;
 import org.springframework.stereotype.Component;
 
 import com.ai.yc.translator.api.allointerper.intefaces.IYCAlloTranslatorServiceSV;
-import com.ai.yc.translator.api.translatorservice.param.AlloTranslatorRequest;
-import com.ai.yc.translator.api.translatorservice.param.AlloTranslatorResponse;
+import com.ai.yc.translator.api.allointerper.param.AlloTranslatorRequest;
+import com.ai.yc.translator.api.allointerper.param.AlloTranslatorResponse;
 import com.alibaba.dubbo.config.annotation.Service;
 @Service(validation = "true")
 @Component
